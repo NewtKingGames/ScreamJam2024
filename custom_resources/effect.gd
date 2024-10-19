1759dd9ca7c8aa1dcbb3472faf8469b8c7a22441
NewtKingGames <newtkinggames@gmail.com>
2024-10-19 15:40:27 -0500

    Add sprite to interactables and make effects single or repeataable

--- custom_resources/effect.gd
+++ custom_resources/effect.gd
@@ -3,7 +3,7 @@ extends Resource
 
 # TODO - think about just extending this class to have hardcoded function name and object types
 @export var method_name: String = "" # TODO see if theres a better way to call the function vame
-
+@export var is_repeatable: bool = true
 
 func perform_action(target: Node2D):
 	# TODO add some null checks?
--- scenes/interactables/interactable_node.gd
+++ scenes/interactables/interactable_node.gd
@@ -1,6 +1,7 @@
 class_name InteractableNode
 extends StaticBody2D
 
+const COMPUTER_DEACTIVATED_SPRITE = preload("res://sprites/computer3.png")
 
 @export var interactable_resource: Interactable
 @export var effect: Effect
@@ -13,6 +14,12 @@ extends StaticBody2D
 # block out the whole screen with it. If we do this then we probably want a global signal and have the interactable
 # emit some kind of resource/scene which the main interaction panel can read and show to the player
 
+var is_interactable: bool = true: 
+	set(value):
+		if value == false:
+			$Sprite2D.texture = COMPUTER_DEACTIVATED_SPRITE
+			interact_label.hide()
+		is_interactable = value
 
 func _ready() -> void:
 	interact_label.hide()
@@ -20,16 +27,18 @@ func _ready() -> void:
 	player_area.body_exited.connect(_on_player_exited_area)
 
 func _on_player_entered_area(body: Node2D) -> void:
-	interact_label.show()
-	var player = body as Player
-	if player:
-		player.entered_interactable_area(self)
+	if is_interactable:
+		interact_label.show()
+		var player = body as Player
+		if player:
+			player.entered_interactable_area(self)
 	
 func _on_player_exited_area(body: Node2D) -> void:
-	interact_label.hide()
-	var player = body as Player
-	if player:
-		player.exited_interactable_area(self)
+	if is_interactable:
+		interact_label.hide()
+		var player = body as Player
+		if player:
+			player.exited_interactable_area(self)
 
 func player_interacted() -> void:
 	Events.player_started_interaction.emit(self)
@@ -37,8 +46,10 @@ func player_interacted() -> void:
 func player_exit_interaction() -> void:
 	Events.player_exited_interaction.emit(self)
 
-
 func do_effect() -> void:
 	if not target:
 		return
 	effect.perform_action(target)
+	if not effect.is_repeatable:
+		is_interactable = false
+	# TODO - add shome check in the effect resource like "is repeatable" and if it's not switch to the deactivated skin
