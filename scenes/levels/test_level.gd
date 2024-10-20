910df43ff4bf26078e48430b75297267c021a471
NewtKingGames <newtkinggames@gmail.com>
2024-10-20 11:35:28 -0500

    add level template scene

--- scenes/levels/test_level.gd
+++ scenes/levels/test_level.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
+
+@export var next_level: PackedScene
+
 @onready var interactable_panel: InteractablePanel = %InteractablePanel
 const ZAPSPLAT_LAB_MACHINE_WHIRR_107536 = preload("res://sounds/zapsplat_lab_machine_whirr_107536.mp3")
 @onready var ambient_noise_player: AudioStreamPlayer = $AmbientNoisePlayer
+
 
 func _ready() -> void:
 	interactable_panel.hide()
@@ -14,3 +18,12 @@ func _on_player_started_interaction(interactable_node: InteractableNode) -> void
 
 func _on_player_exited_interaction(interactable_node: InteractableNode) -> void:
 	interactable_panel.hide_panel()
+
+
+func player_exited_level():
+	print("player exited level")
+	if next_level:
+		await get_tree().create_timer(1).timeout
+		get_tree().change_scene_to_packed(next_level)
+	else:
+		print("game ended")
