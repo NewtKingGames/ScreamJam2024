73094412b93187a30fca8b4ff4fdc02794de9cc0
NewtKingGames <newtkinggames@gmail.com>
2024-10-21 15:52:50 -0500

    add sounds and signals to paper scene

--- scenes/objects/paper_note.gd
+++ scenes/objects/paper_note.gd
@@ -8,8 +8,12 @@ extends Node2D
 @onready var panel: Panel = $CanvasLayer/Panel
 @onready var label: Label = %Label
 @onready var interact_label: Label = $InteractLabel
+@onready var open_sound: AudioStreamPlayer2D = $OpenSound
+@onready var close_sound: AudioStreamPlayer2D = $CloseSound
 
 
+signal paper_opened
+signal paper_closed
 
 var is_player_near: bool = false
 var is_paper_active: bool = false
@@ -23,20 +27,26 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	interact_label.visible = is_player_near
-	if is_player_near and Input.is_action_just_pressed("interact"):
+	if is_player_near and Input.is_action_just_pressed("interact") and not is_paper_active:
 		read_paper()
-	if is_paper_active and Input.is_action_just_pressed("exit"):
+	elif is_paper_active and (Input.is_action_just_pressed("exit") or Input.is_action_just_pressed("interact")):
 		put_away_paper()
 
 func read_paper() -> void:
+	if is_paper_active:
+		return
 	is_paper_active = true
-	# Play noise
 	panel.show()
+	paper_opened.emit()
+	open_sound.play()
 	
 func put_away_paper() -> void:
+	if not is_paper_active:
+		return
 	is_paper_active = false
-	# Play noise
 	panel.hide()
+	paper_closed.emit()
+	close_sound.play()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body is Player:
