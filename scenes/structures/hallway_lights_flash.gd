089400f6f3b7b80c3041bf4d42808572deeafb93
NewtKingGames <newtkinggames@gmail.com>
2024-10-21 17:34:33 -0500

    finish hallway moving light scene

--- scenes/structures/hallway_lights_flash.gd
+++ scenes/structures/hallway_lights_flash.gd
@@ -6,15 +6,32 @@ extends Node2D
 enum LightPattern{STATIC, IN_ORDER, BACK_FORTH}
 
 @export var pattern: LightPattern = LightPattern.STATIC
-@export var light_on_duration: float = 5
-@export var delay_until_next_light: float = 4.5
+@export var light_on_duration: float = 4.5
+@export var delay_until_next_light: float = 4
+var are_all_lights_on: bool = false
 var are_lights_powered: bool = true
 var are_lights_flashing: bool = true
+var light_child_index: int = 0
+var next_light_child_index: int = 0
+var total_light_children: int = 0
+var light_child_index_increment: int = 1
 
 
 func _ready() -> void:
-	toggle_all_lights(are_lights_powered)
-	start_light_pattern()
+	next_light_child_index = get_next_light_index(light_child_index)
+	total_light_children = lights.get_child_count()
+	toggle_all_lights(are_all_lights_on)
+	if are_lights_powered:
+		start_light_pattern()
+
+func move_to_next_lights() -> void:
+	print("getting next lights")
+	light_child_index = next_light_child_index
+	next_light_child_index = get_next_light_index(next_light_child_index)
+	print("new current")
+	print(light_child_index)
+	print("new next")
+	print(next_light_child_index)
 
 func toggle_all_lights(on: bool) -> void:
 	for light in lights.get_children():
@@ -25,7 +42,33 @@ func toggle_all_lights(on: bool) -> void:
 				light.turn_off()
 
 func start_light_pattern() -> void:
-	#var tween = create_tween().set_loops()
-	while true:
-		print("word!")
+	turn_on_light_with_timer_to_off(light_child_index)
+
+func turn_on_light_with_timer_to_off(index: int) -> void:
+	turn_on_light(index)
+	get_tree().create_timer(light_on_duration).timeout.connect(
+		func():
+			turn_off_light(index)
+	)
+	get_tree().create_timer(delay_until_next_light).timeout.connect(
+		func():
+			turn_on_light_with_timer_to_off(get_next_light_index(index))
+	)
+
+func turn_off_light(index: int) -> void:
+	lights.get_children()[index].turn_off()
 	
+func turn_on_light(index: int) -> void:
+	lights.get_children()[index].turn_on()
+
+func get_next_light_index(current_index: int) -> int:
+	if pattern == LightPattern.IN_ORDER:
+		# light_child_index_increment is always 1 here
+		return (current_index + light_child_index_increment) % total_light_children
+	else:
+		if current_index == total_light_children - 1:
+			# If we're on the last index
+			light_child_index_increment = -1
+		elif current_index == 0:
+			light_child_index_increment = 1
+		return current_index + light_child_index_increment
