3637384a00de595fb689f0453299a862df9727c6
NewtKingGames <newtkinggames@gmail.com>
2024-10-21 00:17:45 -0500

    add sounds to light

--- scenes/lights/togglable_light.gd
+++ scenes/lights/togglable_light.gd
@@ -1,6 +1,9 @@
 class_name TogglableLight
 extends PointLight2D
 
+@onready var turn_on_sound: AudioStreamPlayer2D = $TurnOnSound
+@onready var turn_off_sound: AudioStreamPlayer2D = $TurnOffSound
+
 
 func turn_on():
 	toggle_light(true)
@@ -9,6 +12,10 @@ func turn_off():
 	toggle_light(false)
 
 func toggle_light(on: bool):
+	if on:
+		turn_on_sound.play()
+	else:
+		turn_off_sound.play()
 	enabled = on
 	# TODO play sound effect
 	
