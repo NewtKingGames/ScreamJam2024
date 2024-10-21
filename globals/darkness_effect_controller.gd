f40dd4ada6740c5c54bd5a4e04449afc2fe8bb0d
NewtKingGames <newtkinggames@gmail.com>
2024-10-21 10:24:13 -0500

    add darkness effect controller

---
+++ globals/darkness_effect_controller.gd
@@ -0,0 +1,39 @@
+extends Node
+
+var is_active: bool = true
+var tween: Tween
+var camera: Camera2D
+var player: Player
+var camera_starting_zoom: Vector2 # Starts at (0.6, 0.6)
+var camera_darkness_zoom: Vector2 = Vector2(1, 1)
+@onready var canvas_modulate: CanvasModulate = $CanvasModulate
+
+# Effects we want to do 
+# zoom in the camera
+# Possibly play some sounds
+# possibly modulate the whole scene?
+# Make the game even darker?
+
+func _ready() -> void:
+	camera = get_tree().get_first_node_in_group("camera") as Camera2D
+	camera_starting_zoom = camera.zoom
+	player = get_tree().get_first_node_in_group("player")
+	tween = create_tween()
+	Events.player_entered_darkness.connect(_on_player_entered_darkness)
+	Events.player_exited_darkness.connect(_on_player_exited_darkness)
+
+func _on_player_entered_darkness() -> void:
+	print("entering darkness")
+	tween.kill()
+	tween = create_tween()
+	tween.tween_interval(1.0)
+	tween.tween_property(camera, "zoom", camera_darkness_zoom, 5)
+	# What should the effects be?
+
+
+func _on_player_exited_darkness() -> void:
+	print("exiting darkness")
+	tween.kill()
+	tween = create_tween()
+	tween.tween_property(camera, "zoom", camera_starting_zoom, 2.0)
+	pass
